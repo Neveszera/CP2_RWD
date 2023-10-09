8630918004c526fa84daa796d3806953cb4a9301
Neveszera <gabrielgng26@gmail.com>
2023-10-08 21:46:09 -0300

    Fetch realizado no Home, Aparelhos, Inserir e deletar.

--- src/routes/EditarAparelho/index.jsx
+++ src/routes/EditarAparelho/index.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
-import aparelhosData from "../../data/aparelhosData";
 import styles from './index.module.css';
 
 function EditarAparelho() {
